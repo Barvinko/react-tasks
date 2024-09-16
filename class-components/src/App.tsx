30877da28498c73afb77d7d9cd8b929ba0352dd7
Barvinko <vlad.barvinko@gmail.com>
2024-09-16 21:16:24 +0300

    feat: develop API data retrieval

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,11 +1,30 @@
-import MainPage from './pages/MainPage'
-import './App.css'
+import { useState, useEffect } from 'react';
+import { getData } from './utilits/api/getData';
+import MainPage from './pages/MainPage';
+import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    getData('https://swapi.dev/api/people/1/').then((data) => {
+      setData(data);
+    }).catch((error) =>{
+      console.error('Data retrieval error:', error);
+    }).finally(()=>{
+      setLoading(false);
+    }) 
+  }, []);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
   return (
-    <MainPage/>
-  )
+    <>
+      <MainPage />
+      <p>{data ? JSON.stringify(data) : 'No data available'}</p>
+    </>
+  );
 }
 
-export default App
+export default App;
